1504341e8b04db0749a3dc2fc83478dbd9b34859
Raul Moreno <rauledmore98@outlook.com>
2024-10-17 16:40:57 -0500

    Avance de crm ziba

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyBLc_2YBWWUufNU6KwgD_QwFJ2513YJzd8",
-  authDomain: "inventario-ziba424.firebaseapp.com",
-  projectId: "inventario-ziba424",
-  storageBucket: "inventario-ziba424.appspot.com",
-  messagingSenderId: "46736147513",
-  appId: "1:46736147513:web:cddaa283b6faab310bf9e8",
-  measurementId: "G-BR2DHNVP9G",
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
